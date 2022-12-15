1d7136658b081ba0a5030211345ef1ddccd0867a
onedou <onedou@126.com>
2022-12-15 10:15:57 +0800

    chapter3.1

--- www/index.js
+++ www/index.js
@@ -1,24 +1,6 @@
-async function run() {
-    const importObject = {
-        console: {
-            log: () => {
-                console.log("log info!");
-            },
-            error: () => {
-                console.log("error info");
-            }
-        }
-    }
+import init, { hello } from "wasm_game";
 
-    const response = await fetch("test1.wasm");
-    const buffer = await response.arrayBuffer();
-    debugger;
-    const wasm = await WebAssembly.instantiate(buffer, importObject);
-
-    const addFunction = wasm.instance.exports.add;
-    const result = addFunction(10, 20);
-
-    console.log(result);
-}
-
-run();
+init().then(() => {
+    console.log("ok!!!!");
+    hello("test");
+})
